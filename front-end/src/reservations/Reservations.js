d82069d51707fedaf6bb8764d0577334351d0acc
Kelvin He <zhe2705@gmail.com>
2023-02-06 20:53:05 -0500

    Status frontend display WIP

--- front-end/src/reservations/Reservations.js
+++ front-end/src/reservations/Reservations.js
@@ -3,7 +3,7 @@ import { formatAs12Hr } from "../utils/date-time"
 
 function Reservations({reservations}){
     const rows = reservations.map((reservation) => {
-        const {reservation_id,first_name,last_name,mobile_number,reservation_time,people} = reservation
+        const {reservation_id,first_name,last_name,mobile_number,reservation_time,people,status} = reservation
         return (
         <tr key={reservation_id}>
             <td className="h-25 px-3">{reservation_id}</td>
@@ -11,7 +11,8 @@ function Reservations({reservations}){
             <td className="h-25 px-3">{mobile_number}</td>
             <td className="h-25 px-3">{formatAs12Hr(reservation_time)}</td>
             <td className="h-25 px-3">{people}</td>
-            <td><a href={`/reservations/${reservation_id}/seat`}><button>Seat</button></a></td>
+            <td className="h-25 px-3">{status[0].toUpperCase() + status.slice(1)}</td>
+            <td className="h-25 px-3">{status === "booked" ? <a href={`/reservations/${reservation_id}/seat`}><button>Seat</button></a> : null}</td>
         </tr>
     )})
 
@@ -24,6 +25,7 @@ function Reservations({reservations}){
                     <th className="px-3">Phone</th>
                     <th className="px-3">Time</th>
                     <th className="px-3">People</th>
+                    <th className="px-3">Status</th>
                 </tr>
             </thead>
             <tbody>{rows}</tbody>
