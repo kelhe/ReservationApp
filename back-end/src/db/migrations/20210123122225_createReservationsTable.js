e2e0166f4098e89f0f1f0cb9652c8dc8e2048cbe
Kelvin He <zhe2705@gmail.com>
2023-02-08 17:15:57 -0500

    fixed testing bug

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,5 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
+    table.increments("reservation_id").primary();
     table.string("first_name").notNullable();
     table.string("last_name").notNullable();
     table.varchar("mobile_number").notNullable();
@@ -7,7 +8,6 @@ exports.up = function (knex) {
     table.time("reservation_time").notNullable();
     table.integer("people").notNullable();
     table.timestamps(true, true);
-    table.increments("reservation_id").primary();
   });
 };
 
